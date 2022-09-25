3aba8db6e136c149d5764130e95ed24c368af3ab
Sasank Tadepalli <saisasank25@gmail.com>
2022-09-25 14:41:22 -0700

    Implemented Basic create() functionality

--- FileSystem.py
+++ FileSystem.py
@@ -7,25 +7,26 @@ class FileSchema:
 	forwardPointer = 0
 	userData = ""
 
-	def __init__(self, backPointer, forwardPointer, userData):
+	def __init__(self, backPointer = 0, forwardPointer = 0, userData = ""):
 		self.backPointer = backPointer
 		self.forwardPointer = forwardPointer
 		self.userData = userData
-
-class BlockSchema:
-
-	blockType = "F"
-	fileName = ""
-	link = 0
-	size = 0
 
 class DirectorySchema:
 
 	backPointer = 0
 	forwardPointer = 0
-	free = 1
+	free = 0
 	filler = 0
-	directories = [BlockSchema() for _ in range(31)]
+	directories = None
+
+	def __init__(self):
+		self.directories = [{
+			"blockType": "F",
+			"fileName": "",
+			"link": 0,
+			"size": 0
+		} for _ in range(31)]
 
 # file = FileSchema(100, 200, "hello")
 # dirs = DirectorySchema()
@@ -36,28 +37,40 @@ class FileSystem:
 	memoryBlocks[0] = root
 
 	def create(self, blockType, fileName):
-		if blockType == "D":
-			currentDir = self.root
-			filePaths = fileName.split("/")
-			for filePath in filePaths[1:-1]:
-				for directory in currentDir.directories:
-					if directory.fileName == filePath:
-						currentDir = memoryBlocks[directory.link]
-						break
+		currentDir = self.root
+		filePaths = fileName.split("/")
+		for filePath in filePaths[1:-1]:
 			for directory in currentDir.directories:
-				if directory.blockType == "F":
-					directory.blockType = "D"
-					directory.fileName = filePaths[-1]
-					print(filePaths[-1])
-					directory.link = self.root.free
-					memoryBlocks[self.root.free] = DirectorySchema()
+				if directory["fileName"] == filePath:
+					currentDir = memoryBlocks[directory["link"]]
+					break
+		if blockType == "D":
+			for directory in currentDir.directories:
+				if directory["blockType"] == "F":
+					# print(directory["link"], directory["blockType"], directory["fileName"], directory["size"])
+					directory["blockType"] = "D"
+					directory["fileName"] = filePaths[-1]
 					self.root.free = freeBlockList.pop()
+					directory["link"] = self.root.free
+					memoryBlocks[self.root.free] = DirectorySchema()
+					break
+		if blockType == "U":
+			for directory in currentDir.directories:
+				if directory["blockType"] == "F":
+					# print(directory["link"], directory["blockType"], directory["fileName"], directory["size"])
+					directory["blockType"] = "U"
+					directory["fileName"] = filePaths[-1]
+					self.root.free = freeBlockList.pop()
+					directory["link"] = self.root.free
+					memoryBlocks[self.root.free] = FileSchema()
 					break
 
 fileSystem = FileSystem()
-# print(memoryBlocks)
-print(freeBlockList)
 fileSystem.create("D", "root/dir1")
-# print(memoryBlocks[0].directories[0].fileName)
 fileSystem.create("D", "root/dir1/dir2")
+# print(memoryBlocks[1].directories[0]["fileName"])
+fileSystem.create("D", "root/dir1/dir3")
+fileSystem.create("U", "root/dir1/file1")
+print(memoryBlocks[1].directories)
+# print(memoryBlocks[0].directories)
 print(memoryBlocks)
