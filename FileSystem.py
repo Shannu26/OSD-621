46b92c1a4612ade5206b30358095c2dd3220014b
Sasank Tadepalli <saisasank25@gmail.com>
2022-09-26 17:23:23 -0700

    Update FileSystem.py

--- FileSystem.py
+++ FileSystem.py
@@ -60,6 +60,30 @@ class FileSystem:
 					memoryBlocks[self.root.free] = FileSchema()
 					break
 
+	def delete(self, fileName):
+		currentDir = self.root
+		fileLink = -1
+		filePaths = fileName.split("/")
+		for filePath in filePaths[1:-1]:
+			for directory in currentDir.directories:
+				if directory["fileName"] == filePath:
+					currentDir = memoryBlocks[directory["link"]]
+					break
+		for directory in currentDir.directories:
+			if directory["blockType"] == "U" and directory["fileName"] == filePaths[-1]:
+				fileLink = directory["link"]
+				directory["blockType"] = "F"
+				directory["fileName"] = ""
+				directory["link"] = 0
+				break
+
+		while fileLink:
+			freeBlockList.add(fileLink)
+			newFileLink = memoryBlocks[fileLink].forwardPointer
+			memoryBlocks[fileLink] = None
+			fileLink = newFileLink
+			
+
 	def open(self, mode, fileName):
 		if self.openedFile:
 			print("Another file is already open. Please close that file to open new file")
@@ -77,7 +101,8 @@ class FileSystem:
 				self.openedFile = {
 					"mode": mode,
 					"name": fileName,
-					"link": memoryBlocks[directory["link"]]
+					"link": directory["link"]
+					# "link": memoryBlocks[directory["link"]]
 				}
 				break
 
@@ -85,12 +110,45 @@ class FileSystem:
 		if self.openedFile:
 			self.openedFile = None
 
+	def read(self, n):
+		fileLink = self.openedFile["link"]
+		howManyFilesRead = 0
+		i = 0
+		while howManyFilesRead * 256 + i < n:
+			if i == 256:
+				if memoryBlocks[fileLink].forwardPointer:
+					fileLink = memoryBlocks[fileLink].forwardPointer
+					howManyFilesRead += 1
+					i = 0
+				else: 
+					print("EOF")
+					return
+			else:
+				if i == len(memoryBlocks[fileLink].userData): 
+					print("EOF")
+					return 
+				print(memoryBlocks[fileLink].userData[i], end="")
+				i += 1
+		print()
+
 	def write(self, n, data):
+		fileLink = self.openedFile["link"]
 		if len(data) < n:
 			data = data + " " * (n - len(data))
 		else:
 			data = data[:n]
-		self.openedFile["link"].userData = data
+
+		i = 0
+		while i < n:
+			if len(memoryBlocks[fileLink].userData) == 256:
+				self.root.free = freeBlockList.pop()
+				memoryBlocks[fileLink].forwardPointer = self.root.free
+				memoryBlocks[self.root.free] = FileSchema(fileLink)
+				fileLink = self.root.free
+				continue
+			memoryBlocks[fileLink].userData += data[i]
+			i += 1
+		# memoryBlocks[self.openedFile["link"]].userData = data
 
 fileSystem = FileSystem()
 fileSystem.create("D", "root/dir1")
@@ -104,12 +162,18 @@ print(memoryBlocks)
 print(fileSystem.openedFile)
 fileSystem.open("I", "root/dir1/file1")
 print(fileSystem.openedFile)
-fileSystem.write(5, "1234567890")
-print(memoryBlocks[4].userData)
+fileSystem.write(600, "1" * 700)
+# print(memoryBlocks[4].userData)
+# print(memoryBlocks[5].userData)
+# print(memoryBlocks[6].userData)
+fileSystem.read(601)
 fileSystem.close()
 print(fileSystem.openedFile)
+print(memoryBlocks)
 
-
+fileSystem.delete("root/dir1/file1")
+print(memoryBlocks)
+print(freeBlockList)
 
 
 
